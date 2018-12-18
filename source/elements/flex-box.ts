4da6a72f4b73c11721df6aff9f28e60263f8b949
Teffen Ellis <teffen@nirri.us>
2018-12-18 18:58:05 -0500

    Update.

--- source/elements/flex-box.ts
+++ source/elements/flex-box.ts
@@ -1,35 +1,56 @@
-import createElement from '../create-element';
+import createElement from '../create-element'
 
 interface FlexBoxAttributes {
-  flex: string
-  grow: number
-  wrap: boolean
-  alignItems: 'normal' | "stretch" | "center" | "start" | "end" | "flex-start" | "flex-end" | "self-start" | "self-end" | "baseline" | "first baseline" | "last" | "safe" | "unsafe" | "inherit" | "initial" | "unset" | string
-  shrink: number
-  basis: string
+  flex?: string
+  grow?: number
+  wrap?: boolean
+  alignItems?:
+    | 'normal'
+    | 'stretch'
+    | 'center'
+    | 'start'
+    | 'end'
+    | 'flex-start'
+    | 'flex-end'
+    | 'self-start'
+    | 'self-end'
+    | 'baseline'
+    | 'last'
+    | 'safe'
+    | 'unsafe'
+    | 'inherit'
+    | 'initial'
+    | 'unset'
+  shrink?: number
+  basis?: string
 }
 
-const FlexBox = createElement('flex-box', {
-  attributes: {
-    'flex',
-    'grow',
-    'basis'
-    {}
-  ]
+const foo: FlexBoxAttributes = {
+  alignItems: 'flex-start'
+}
+
+const FlexBox = createElement<FlexBoxAttributes>('flex-box', {
+  styles(css) {
+    return css`
+      :host {
+        display: flex;
+      }
+    `
+  },
+  template(html) {
+    // Hypothetical
+    function pluckKeys(): any {}
+    const { attributes } = this
+
+    Object.assign(this.shadowRoot.styles, pluckKeys(attributes, 'alignItems', 'etc...'))
+
+    return html`
+      <div><slot></slot></div>
+    `
+  },
   lifecycle: {
     beforeElementInserted() {
       console.log(this.elementName)
     }
   }
 })
-
-
-
-const {currentScript} = document
-
-if (currentScript) {
-  const providedElementName = currentScript.getAttribute('element-name')
-}
-
-MyApp.register()
-FlexBox.register()
