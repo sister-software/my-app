e2058ad33ecf17b05d5f808e272d94c6d0b7e786
Teffen Ellis <teffen@nirri.us>
2018-12-18 20:20:33 -0500

    Flesh out constructor.

--- source/create-element.ts
+++ source/create-element.ts
@@ -1,10 +1,14 @@
-import MyElement from './elements/my-element.js'
+import MyElement, { ParseCSS, ParseHTML } from './elements/my-element.js'
 import createNoop from './helpers/noop.js'
+import camelToKebab from './helpers/camel-to-kebab.js'
+import kebabToPascal from './helpers/kebab-to-pascal.js'
+import { TemplateResult } from '../node_modules/lit-html/lit-html.js'
 
 export interface MyElementAttribute {
   name: string
   private: boolean
 }
+
 export interface MyElementLifeCycle {
   /**
    * Callback executed before an element is inserted in the document.
@@ -31,12 +35,57 @@ const defaultLifeCycleHandlers: MyElementLifeCycle = {
   afterElementRemoved: createNoop()
 }
 
-export interface CreateMyElementOptions {
-  attributes?: string | MyElementAttribute
-  // styles?: css
-  lifecycle?: Partial<MyElementLifeCycle>
+interface CustomElement {
+  styles?: (css: ParseCSS) => string
+  template(html: ParseHTML): TemplateResult
 }
 
-function createElement(elementName: string, options: CreateMyElementOptions = {}): MyElement {}
+export interface CreateMyElementOptions {
+  attributes?: string | MyElementAttribute
+  styles?: CustomElement['styles']
+  template: CustomElement['template']
+  lifecycle?: Partial<MyElementLifeCycle>
+  shadowRoot?: ShadowRootInit
+}
+
+// A round-about way of making custom classes with a dynamic constructor name
+function createElementClass<T>(name: string, BaseClass: Function, prototype: T): T {
+  const Klass = new Function(
+    'name',
+    'BaseClass',
+    `
+    return class ${name} extends BaseClass {}
+  `
+  )(kebabToPascal(name), BaseClass)
+
+  Klass.prototype = prototype
+
+  return Klass
+}
+
+/**
+ *
+ * @param elementName A unique name with dashes-between-words, or camelCased.
+ * @param options Options describing your element's behavior.
+ */
+function createElement<A>(elementName: string, options: CreateMyElementOptions): CustomElement {
+  elementName = camelToKebab(elementName)
+
+  if (elementName.indexOf('-') === -1) {
+    throw new Error(
+      `${elementName}: Custom elements must have a dash (-) in their name to avoid overlap with pre-existing elements.`
+    )
+  }
+
+  if (options.attributes) {
+    options.attributes
+  }
+
+  const CustomElement = createElementClass<CustomElement>(elementName, MyElement, {
+    template: options.template
+  })
+
+  return CustomElement
+}
 
 export default createElement
